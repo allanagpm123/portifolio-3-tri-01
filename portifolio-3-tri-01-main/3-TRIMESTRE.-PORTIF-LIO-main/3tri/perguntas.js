d5c85e73efb2546e5152b9e099ea79aeecc84163
allanagpm123 <132069655+allanagpm123@users.noreply.github.com>
2024-11-14 08:46:45 -0300

    Add files via upload

---
+++ portifolio-3-tri-01-main/3-TRIMESTRE.-PORTIF-LIO-main/3tri/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Regras',
+    'O que é um set?',
+    'Um set é uma parte do jogo, e uma equipe precisa ganhar 25 pontos (com pelo menos 2 pontos de diferença) para vencer o set'
+)
+
+criaCartao(
+    'Posições',
+    'Qual a função do levantador?',
+    'Organiza as jogadas, fazendo passes precisos para os atacantes. Ele é o "cérebro" da equipe.'
+)
+
+criaCartao(
+    'Posições',
+    'Qual a função do líbero?',
+    'É um jogador defensivo que se especializa em recepção e defesa. Não pode atacar nem sacar, e não usa a mesma camisa dos outros jogadores.'
+)
+
+criaCartao(
+    'Regras',
+    'O que caracteriza uma falta de ataque?',
+    'Uma falta de ataque ocorre quando um jogador ataca a bola da linha de três metros (linha de ataque) e pisa na linha ou ultrapassa-a.    '
+)
